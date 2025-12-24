0251f4eb37c074e67a8f2a904a337fde67dae79c
fabiooo4 <fabioirimie@gmail.com>
2025-12-24 15:31:14 +0100

    13  2025-12-24  13:11:15  26.05.20251221.a653104  6.12.63

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Commit for keyboard shortcuts options
     zen-browser.url = "github:0xc000022070/zen-browser-flake/85ef44c";
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";
     zen-browser.inputs.home-manager.follows = "home-manager";
