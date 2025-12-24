04f4555c516d6dcbedd299e76037b9044e9884d6
fabiooo4 <fabioirimie@gmail.com>
2025-12-24 16:08:48 +0100

    13  2025-12-24  13:11:15  26.05.20251221.a653104  6.12.63

--- modules/home-manager/browser/zen/zen.nix
+++ modules/home-manager/browser/zen/zen.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   inputs,
   userSettings,
   ...
@@ -117,6 +118,83 @@
         pinsForce = true;
         containersForce = true;
         inherit containers spaces pins;
+
+        settings = {
+          zen.view.compact = {
+            hide-toolbar = true;
+          };
+        };
+
+        search = {
+          force = true;
+          default = "google";
+          engines = let
+            nixSnowflakeIcon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+          in {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    {
+                      name = "type";
+                      value = "packages";
+                    }
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = nixSnowflakeIcon;
+              definedAliases = ["np"];
+            };
+            "Nix Options" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/options";
+                  params = [
+                    {
+                      name = "channel";
+                      value = "unstable";
+                    }
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                  ];
+                }
+              ];
+              icon = nixSnowflakeIcon;
+              definedAliases = ["nop"];
+            };
+            "Home Manager Options" = {
+              urls = [
+                {
+                  template = "https://home-manager-options.extranix.com/";
+                  params = [
+                    {
+                      name = "query";
+                      value = "{searchTerms}";
+                    }
+                    {
+                      name = "release";
+                      value = "master"; # unstable
+                    }
+                  ];
+                }
+              ];
+              icon = nixSnowflakeIcon;
+              definedAliases = ["hmop"];
+            };
+            bing.metaData.hidden = "true";
+          };
+        };
       };
     };
   };
