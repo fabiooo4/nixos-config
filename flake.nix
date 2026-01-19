9dd392fb5b92c3d3ea135ad04e00941611dace9c
fabiooo4 <fabioirimie@gmail.com>
2026-01-19 11:15:51 +0100

    feat: added backup for existing files outside nix

--- flake.nix
+++ flake.nix
@@ -52,6 +52,7 @@
 
               inputs.home-manager.nixosModules.home-manager
               {
+                home-manager.backupFileExtension = "backup";
                 home-manager.useGlobalPkgs = true;
                 home-manager.useUserPackages = true;
                 home-manager.extraSpecialArgs = {
