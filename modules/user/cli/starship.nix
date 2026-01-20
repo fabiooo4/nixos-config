8aff5f44cb7eeabc75583b90c4c74be63a5b9d75
fabiooo4 <fabioirimie@gmail.com>
2026-01-20 20:07:49 +0100

    feat: added starship stylix colorscheme

--- modules/user/cli/starship.nix
+++ modules/user/cli/starship.nix
@@ -3,24 +3,53 @@
   config,
   ...
 }: {
-  config = {
+  options = {
+    userSettings.cli.starship = {
+      colorscheme = lib.mkOption {
+        type = lib.types.enum ["base16" "gruvbox_dark"];
+        default = "base16";
+        description = "The colorscheme to use for the starship prompt.";
+      };
+    };
+  };
+
+  config = let
+    cfg = config.userSettings.cli.starship;
+  in {
     programs.starship = lib.mkIf (!config.userSettings.dotfiles.kitty.enable) {
       enable = true;
 
       settings = {
-        palette = lib.mkForce "gruvbox_dark";
-        palettes.gruvbox_dark = lib.mkForce {
-          color_fg0 = "#fbf1c7";
-          color_bg1 = "#3c3836";
-          color_bg3 = "#665c54";
-          color_blue = "#458588";
-          color_aqua = "#689d6a";
-          color_green = "#98971a";
-          color_orange = "#d65d0e";
-          color_purple = "#b16286";
-          color_red = "#cc241d";
-          color_yellow = "#d79921";
-        };
+        palette = cfg.colorscheme;
+
+        palettes.base16 = let
+          colors = config.lib.stylix.colors;
+        in
+          if config.stylix.enable && cfg.colorscheme == "base16"
+          then {
+            color_fg0 = "#${colors.base05}";
+            color_bg1 = "#${colors.base01}";
+            color_bg3 = "#${colors.base02}";
+            color_blue = "#${colors.base0D}";
+            color_aqua = "#${colors.base0C}";
+            color_green = "#${colors.base0B}";
+            color_orange = "#${colors.base09}";
+            color_purple = "#${colors.base0E}";
+            color_red = "#${colors.base08}";
+            color_yellow = "#${colors.base0A}";
+          }
+          else {
+            color_fg0 = "#fbf1c7";
+            color_bg1 = "#3c3836";
+            color_bg3 = "#665c54";
+            color_blue = "#458588";
+            color_aqua = "#689d6a";
+            color_green = "#98971a";
+            color_orange = "#d65d0e";
+            color_purple = "#b16286";
+            color_red = "#cc241d";
+            color_yellow = "#d79921";
+          };
 
         format = builtins.concatStringsSep "" [
           "[\\\[](color_red bold)"
