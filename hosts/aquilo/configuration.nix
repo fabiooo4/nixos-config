d2c803f30d51807ad689cf2fd38909c9618e0ba9
fabiooo4 <fabioirimie@gmail.com>
2026-01-26 11:37:57 +0100

    style: changed aquilo wallpaper

--- hosts/aquilo/configuration.nix
+++ hosts/aquilo/configuration.nix
@@ -1,5 +1,14 @@
-{...}: {
+{pkgs, ...}: {
   config = {
+    theme = {
+      gnome-default = {
+        stylix.wallpaper = pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/fabiooo4/wallpapers/main/wallhaven-gw55w7_1920x1080.png";
+          hash = "sha256-UOryQneCfMJVH2QXmxI5exSGttemy94qO2zU879viaY=";
+        };
+      };
+    };
+
     systemSettings = {
       users = ["fabibo"];
       adminUsers = ["fabibo"];
