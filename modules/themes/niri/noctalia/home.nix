767762bb664cf42a21fa96d83460fd4db19298c1
fabiooo4 <fabioirimie@gmail.com>
2026-01-26 22:09:52 +0100

    feat: added noctalia niri template

--- modules/themes/niri/noctalia/home.nix
+++ modules/themes/niri/noctalia/home.nix
@@ -51,6 +51,14 @@
         };
       };
 
+      # Ensure the file is present
+      # home.activation.createNiriColors = lib.hm.dag.entryAfter ["writeBoundary"] ''
+      #   path="${config.home.homeDirectory}/.config/niri/noctalia.kdl"
+      #   if [ ! -f "$path" ]; then
+      #     touch "$path"
+      #   fi
+      # '';
+
       # To see the diff of current settings and the gui modified ones run:
       # nix shell nixpkgs#jq nixpkgs#colordiff nixpkgs#wl-clipboard -c bash -c "diff -u <(jq -S . ~/.config/noctalia/settings.json) <(wl-paste | jq -S .) | colordiff"
       programs.noctalia-shell = {
@@ -524,8 +532,13 @@
           };
 
           templates = {
-            activeTemplates = [];
-            enableUserTheming = false;
+            activeTemplates = [
+              {
+                id = "niri";
+                active = true;
+              }
+            ];
+            enableUserTheming = true;
           };
 
           nightLight = {
@@ -569,6 +582,7 @@
               ];
             }
           ];
+
           window-rules = [
             {
               geometry-corner-radius = {
@@ -634,5 +648,50 @@
           };
         };
       };
+
+      # niri-flake does not support config includes yet
+      # TODO: replace with proper config includes after https://github.com/sodiboo/niri-flake/pull/1548 merge
+      xdg.configFile = (
+        let
+          override = true;
+          originalFileName = "niri-flake";
+          filesToInclude = ["noctalia"];
+
+          withOriginalConfig = dmsFiles:
+            if override
+            then [originalFileName] ++ dmsFiles
+            else dmsFiles ++ [originalFileName];
+
+          fixes = map (fix: "\n${fix}") (
+            lib.optional true
+            # kdl
+            ''
+              // Border fix
+              // See https://yalter.github.io/niri/Configuration%3A-Include.html#border-special-case for details
+              layout { border { on; }; }
+            ''
+          );
+        in {
+          niri-config.target = lib.mkForce "niri/${originalFileName}.kdl";
+          niri-config-dms = {
+            target = "niri/config.kdl";
+            text = lib.pipe filesToInclude [
+              (map (filename: "${filename}"))
+              withOriginalConfig
+              (map (filename: "include \"${filename}.kdl\""))
+              (files: files ++ fixes)
+              (builtins.concatStringsSep "\n")
+            ];
+          };
+        }
+      );
+
+      # Ensure the included file exists
+      home.activation.createNiriColors = lib.hm.dag.entryAfter ["writeBoundary"] ''
+        path="${config.home.homeDirectory}/.config/niri/noctalia.kdl"
+        if [ ! -f "$path" ]; then
+          touch "$path"
+        fi
+      '';
     };
 }
