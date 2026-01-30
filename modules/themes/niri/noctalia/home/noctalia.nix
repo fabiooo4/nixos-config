447f54b2b9c21c81a991d004c78759a8cbc8bad7
fabiooo4 <fabioirimie@gmail.com>
2026-01-30 23:35:53 +0100

    feat: added more niri keybinds

--- modules/themes/niri/noctalia/home/noctalia.nix
+++ modules/themes/niri/noctalia/home/noctalia.nix
@@ -41,10 +41,28 @@
 
       # Noctalia niri keybinds
       programs.niri.settings.binds = {
+        # Utilities ----------------------------------------
         "Mod+Space".action.spawn = noctalia "launcher toggle";
+        "Mod+C".action.spawn = noctalia "launcher clipboard";
+        "Mod+N".action.spawn = noctalia "notifications dismissAll";
+
+        # Audio controls
         "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease";
         "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase";
         "XF86AudioMute".action.spawn = noctalia "volume muteOutput";
+        "XF86AudioMicMute".action.spawn = noctalia "volume muteInput";
+
+        "XF86AudioPlay".action.spawn = noctalia "media playPause";
+        "XF86AudioStop".action.spawn = noctalia "media stop";
+        "XF86AudioPrev".action.spawn = noctalia "media previous";
+        "XF86AudioNext".action.spawn = noctalia "media next";
+        # Audio controls
+
+        # Brightness controls
+        "XF86MonBrightnessUp".action.spawn = noctalia "brightness increase";
+        "XF86MonBrightnessDown".action.spawn = noctalia "brightness decrease";
+        # Brightness controls
+        # Utilities ----------------------------------------
       };
 
       # To see the diff of current settings and the gui modified ones run:
